2155fe431e48fbbfa92d182bf9e27fe1ac11e70d
JairoSL <jairo9slopez@gmail.com>
2019-01-10 13:03:12 +0100

    commit

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -15,7 +15,7 @@ public class ClaseString {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      String s = "Iesmaralboran";
     }
     
 }
